92cc7224436f0068e2208961ef02bc5612b4fa88
Danilo Hernandez <dhernandez0032@gmail.com>
2022-03-13 08:50:31 -0600

    Configuración de comandos y creación de Design System

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,38 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./public/index.html", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        sanFrancisco: "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/sanFrancisco.jpg')",
+        sanFranciscoDesktop: "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/sanFranciscoDesktop.jpg')",
+        yosemite: "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/yosemite.jpg')",
+        LA: "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/LA.jpg')",
+        seattle: "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/seattle.jpg')",
+        newYork: "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/new_york.jpg')",
+        norway: "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/norway.jpg')",
+        sydney: "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/sydney.jpg')",
+        miami: "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/miami.jpg')",
+        switzerland: "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/switzerland.jpg')",
+        bali: "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/bali.jpg')",
+        chicago: "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/chicago.jpg')",
+        europe: "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/europe.jpg')",
+        iceland: "url('https://raw.githubusercontent.com/platzi/PlatziTravel/main/public/img/iceland.jpg')",
+      },
+      backgroundColor: (theme) => ({
+        ...theme("colors"),
+        primary: "#CC2D4A",
+        secondary: "#8FA206",
+        terciary: "#61AEC9",
+      }),
+      textColor: {
+        primary: "#CC2D4A",
+        secondary: "#8FA206",
+        terciary: "#61AEC9",
+      },
+      fontFamily: {
+        Montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
